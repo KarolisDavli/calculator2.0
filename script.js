67698756e8c5737099ef7bedcdbb4d7e3b8dbbd1
Karolis Davli <kdavlijevas@gmail.com>
2023-06-20 08:53:26 +0200

    basic functionality

--- script.js
+++ script.js
@@ -1,24 +1,64 @@
-const screen = document.getElementsByClassName("screen");
+let firstNumber, operator, secondNumber;
+let displayValue = "";
+
+const screen = document.querySelector(".screen");
+const opBtn = document.querySelectorAll(".op");
 
 // Math buttons
-const addBtn = document.getElementById("add");
-addBtn.addEventListener("click", add);
+// const addBtn = document.getElementById("add");
+// addBtn.addEventListener("click", add);
 
 let brains = [];
 
-function putToBrains(e) {
-  console.log(e.target.textContent);
+function renderOnScreen(e) {
+  displayValue += e.target.textContent;
+  screen.innerHTML = displayValue;
 }
+
+const opSelected = function (e) {
+  if (!firstNumber) {
+    secondNumber == e.target.textContent;
+    console.log(secondNumber);
+  }
+  operator = e.target.textContent;
+  firstNumber = displayValue;
+  console.log(operator, firstNumber);
+};
+
+Array.from(opBtn).map((btn) => {
+  btn.addEventListener("click", opSelected);
+});
 
 const buttons = document.querySelectorAll(".btn");
 
 buttons.forEach((btn) => {
-  btn.addEventListener("click", putToBrains);
+  btn.addEventListener("click", renderOnScreen);
 });
 
-function add(a, b) {
-  console.log(a + b);
-  console.log("Time to add");
-}
+const add = function (a, b) {
+  return a + b;
+};
 
-console.log(addBtn);
+const subtract = function (a, b) {
+  return a - b;
+};
+
+const multiply = function (a, b) {
+  return a * b;
+};
+
+const divide = function (a, b) {
+  return a / b;
+};
+
+const operate = function (x, y, operator) {
+  if (operator == "+") {
+    return add(x, y);
+  } else if (operator == "-") {
+    return subtract(x, y);
+  } else if (operator == "*") {
+    return multiply(x, y);
+  } else if (operator == "/") {
+    return subtract(x, y);
+  }
+};
